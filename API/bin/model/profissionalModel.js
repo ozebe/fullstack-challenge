f8f02a9403fa02a2edd4edfac3f549201d77e7c2
ozebe <wesleysantos345@hotmail.com>
2021-10-09 02:04:33 -0300

    Ao aplicar esse commit, corrige o model de profissional.

--- API/bin/model/profissionalModel.js
+++ API/bin/model/profissionalModel.js
@@ -12,11 +12,11 @@ exports.getProfissionalById = function(id){
 }
 
 exports.saveProfissional = function(profissional){
-    return database.query('insert into fullstackChallenge.profissional (nome, telefone, email, tipoProfissional, situacao, updatedAt, createdAt) values ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) returning *', [profissional.nome, profissional.telefone, profissional.email, profissional.tipoProfissional, profissional.situacao]);
+    return database.query('insert into fullstackChallenge.profissional (nome, telefone, email, tipoprofissional, situacao, updatedAt, createdAt) values ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) returning *', [profissional.nome, profissional.telefone, profissional.email, profissional.tipoprofissional, profissional.situacao]);
 };
 
 exports.updateProfissional = function(id, profissional){
-    return database.query('update fullstackChallenge.profissional set nome = $1, telefone = $2, email = $3, tipoProfissional = $4, situacao = $5, updatedAt = current_timestamp where id = $6 returning *', [profissional.nome, profissional.telefone, profissional.email, profissional.tipoProfissional, profissional.situacao, id]);
+    return database.query('update fullstackChallenge.profissional set nome = $1, telefone = $2, email = $3, tipoprofissional = $4, situacao = $5, updatedAt = current_timestamp where id = $6 returning *', [profissional.nome, profissional.telefone, profissional.email, profissional.tipoProfissional, profissional.situacao, id]);
 }
 
 exports.deleteProfissional = function(id){
