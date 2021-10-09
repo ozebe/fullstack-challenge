f8c1739ba6dfadf879e5019a33a6bc42fe806679
ozebe <wesleysantos345@hotmail.com>
2021-10-09 15:41:16 -0300

    Se aplicado, esse commit irá alterar o RES das solicitações, renderizando as páginas com EJS e bootstrap.

--- API/routes/profissionalRoute.js
+++ API/routes/profissionalRoute.js
@@ -7,19 +7,20 @@ const router = express.Router();
 const profissionalController = require('../bin/controller/profissionalController');
 const tipoProfissionalController = require('../bin/controller/tipoProfissionalController');
 
-//quando ocorrer uma solicitação GET para encontrar todos os funcionários.
+//quando ocorrer uma solicitação GET para encontrar todos os profissionais.
 router.get('/profissional', async function(req, res, next){
     const profissional = await profissionalController.getProfissional();
-    //res.json(profissional.rows);
-    res.render('./profissional/index', { title: 'Profissionais', data: profissional.rows});
+
+    //renderiza a pagina de visualização de todos os profissionais.
+    res.render('./profissional/index', { title: 'Profissionais', data: profissional.rows, message: null, type: 'alert-success'});
 });
 
-//quando ocorrer uma solicitação GET para adicionar um novo usuário.
+//quando ocorrer uma solicitação GET para adicionar um novo profissional.
 router.get('/profissional/add', async function(req, res, next){
     const tipoProfissional = await tipoProfissionalController.getTipoProfissional();
-    //const profissional = await profissionalController.getProfissional();
-    //res.json(profissional.rows);
-    res.render('./profissional/add', { title: 'Adicionar novo profissional', data: tipoProfissional.rows});
+
+    //renderiza a pagina de inclusão de profissional.
+    res.render('./profissional/add', { title: 'Adicionar novo profissional', data: tipoProfissional.rows, message: null});
 });
 
 router.get('/profissional/:id', async function(req, res){
@@ -30,7 +31,24 @@ router.get('/profissional/:id', async function(req, res){
 router.post('/profissional', async function(req, res){
     const profissional = req.body;
     const newProfissional = await profissionalController.saveProfissional(profissional);
-    res.json(newProfissional);
+    const tipoProfissional = await tipoProfissionalController.getTipoProfissional(); //tipos de profissionais
+
+    //se ao tentar inserir o profissional, retornar um erro.
+    if(newProfissional.code){
+        //se o erro for referente a duplicidade.
+        if(newProfissional.code == '23505'){
+            res.render('./profissional/add', { title: 'Profissionais', data: tipoProfissional.rows, message: 'Não foi possível salvar, existem dados duplicados: ' + newProfissional.detail, type: 'alert-danger'});
+        
+        //se o erro for referente a outra condição
+        }else{
+            res.render('./profissional/add', { title: 'Profissionais', data: tipoProfissional.rows, message: 'Erro crítico: ' + newProfissional.detail, type: 'alert-danger'});
+        }
+    //se não ocorrer nenhum erro ao inserir o novo profissional.
+    }else{
+        //redireciona para o index de profissionais, buscando todos os cadastrados.
+        const profissionals = await profissionalController.getProfissional();
+        res.render('./profissional/index', { title: 'Profissionais', data: profissionals.rows, message: newProfissional.rows[0].nome + ' adicionado!', type: 'alert-success'});
+    }
 });
 
 router.put('/profissional/:id', async function(req, res){
